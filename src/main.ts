d9e1ef78540b8bd8a4cdabdd0ba72c312fb46389
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2018-06-11 19:27:54 +0900

    on error

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,48 @@
-import { from, Observable, Subject, BehaviorSubject, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { from, Subject, Subscriber } from 'rxjs';
 
 function hoge(): Promise<string> {
-    return new Promise((resolve: Function, reject: Function) => {
+    return new Promise((_: Function, reject: Function) => {
         reject(new Error("hoge"));
     });
 }
 
-function fuga(): Promise<string> {
-    return new Promise((resolve: Function, reject: Function) => {
-        reject(new Error("fuga"));
-    });
+function fromWrapper<T, U>(p: Promise<T>, onSuccess: (v: T) => U) {
+    return from<T>(p).subscribe(onSuccess, (e: Error) => console.error(e));
 }
 
 async function main() {
     const s = new Subject<number>();
-    from(hoge()).subscribe((v) => {
-        console.log(v);
-    })
     s.subscribe((v) => {
+        Subscriber
         console.log(v);
     })
     s.next(1);
     s.next(2);
     s.next(3);
+
+    from(hoge()).subscribe((v) => {
+        console.log(v);
+    }, (e: Error) => {
+        console.log("on error");
+        console.error(e);
+    });
+
+
+    fromWrapper(hoge(), (v) => console.log(v));
+
+    // try {
+    //     from(hoge()).subscribe((v) => {
+    //         console.log(v);
+    //     });
+    // } catch (e) {
+    //     console.log("catched");
+    //     console.error(e);
+    // }
 }
 
-main();
+try {
+    main();
+} catch (e) {
+    console.log("main try catch");
+    console.error(e);
+}
